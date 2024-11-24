28c1c33c330190bf6a3886a73ba6f488b5595e3f
yuyangchen23 <bryanchen0923@gmail.com>
2024-11-23 22:48:21 -0800

    All fragment UI update

--- app/src/main/java/com/example/cmpt362_project/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/cmpt362_project/dashboard/DashboardFragment.kt
@@ -10,24 +10,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.EditText
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.cmpt362_project.R
 import com.example.cmpt362_project.databinding.FragmentDashboardBinding
 import com.example.cmpt362_project.property.Property
 import com.example.cmpt362_project.property.PropertyAdapter
 import com.example.cmpt362_project.property.PropertyViewModel
-import com.google.android.gms.maps.SupportMapFragment
 
 class DashboardFragment : Fragment() {
 
